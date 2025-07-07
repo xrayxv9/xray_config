2a61a26dfc4e20ebfdc73fbc57e65b064421f0ac
xray <xrayxv.5@gmail.com>
2025-07-08 01:29:29 +0200

    new tabline (a lot better) less aestetic for the moment)

--- lua/xray/remap.lua
+++ lua/xray/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 
+vim.keymap.set('n', '<C-b>', function()
+  if vim.o.showtabline == 2 then
+    vim.o.showtabline = 0
+  else
+    vim.o.showtabline = 2
+  end
+end, { noremap = true, silent = true, desc = "Toggle barbar tabline" })
