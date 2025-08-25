5f42f2f638a84afeb8f4774d14b41b6c2668c96d
Clement Morel <cmorel@3G3.42angouleme.fr>
2025-08-25 09:42:17 +0200

    little change on the comment plugin

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -5,7 +5,8 @@ return {
         require('Comment').setup()
 
         -- Remapping pour commenter une ligne en mode normal
-        vim.keymap.set('n', '<C-\\>', '<CMD>lua require("Comment.api").toggle.linewise.current()<CR>', { desc = 'Toggle comment for current line' })
+		vim.keymap.set('n', '<C-\\>', '<CMD>lua require("Comment.api").toggle.linewise.current()<CR>', { desc = 'Toggle comment for current line' })
+        vim.keymap.set('i', '<C-\\>', '<CMD>lua require("Comment.api").toggle.linewise.current()<CR>', { desc = 'Toggle comment for current line' })
 
         -- Remapping pour commenter une sélection en mode visuel
         vim.keymap.set('v', '<C-\\>', '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', { desc = 'Toggle comment for selected block' })
