c5bedf445e4b4508b9f45ec43b0abb6c3c5f80c9
xray <xrayxv.5@gmail.com>
2025-09-01 02:05:41 +0200

    better plugins

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -3,9 +3,21 @@ return {
     config = function()
         require("toggleterm").setup({
             size = 20,
-            open_mapping = [[<C-t>]], -- Raccourci pour ouvrir/fermer le terminal
-            direction = "float",     -- Peut être "vertical", "horizontal", ou "float"
+            open_mapping = [[<C-t>]], -- Terminal flottant
+            direction = "float",
         })
+
+        -- Terminal type "bottom" (comme VS Code)
+        local Terminal = require("toggleterm.terminal").Terminal
+        local bottom_term = Terminal:new({
+            direction = "horizontal", -- split horizontal (en bas)
+        })
+
+        -- Keymap pour ouvrir/fermer le terminal "bottom" avec <C-o>
+        vim.api.nvim_set_keymap("n", "<C-o>", "<cmd>lua require('toggleterm.terminal').Terminal:new({direction='horizontal'}):toggle()<CR>", { noremap = true, silent = true })
+
+		vim.api.nvim_create_user_command("VsTerminal", function()
+		  require('toggleterm.terminal').Terminal:new({direction='horizontal'}):toggle()
+		end, {})
     end,
 }
-
