229ed7d5da4ba956bdd8470881fae201cca63085
xray <morel.clement@proton.me>
2025-10-17 00:09:09 +0200

    corrected the fact that I couldn't comment multiple lines any more

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -5,7 +5,11 @@ return {
         require('Comment').setup()
 
         -- Remapping pour commenter une ligne en mode normal
-		vim.keymap.set({ 'n', 'v', 'i' }, _G.keybinds.comment.comment, '<CMD>lua require("Comment.api").toggle.linewise.current()<CR>', { desc = 'Toggle comment for current line' })
+		vim.keymap.set('n', _G.keybinds.comment.comment, '<CMD>lua require("Comment.api").toggle.linewise.current()<CR>', { desc = 'Toggle comment for current line' })
+        vim.keymap.set('i', _G.keybinds.comment.comment, '<CMD>lua require("Comment.api").toggle.linewise.current()<CR>', { desc = 'Toggle comment for current line' })
+
+        -- Remapping pour commenter une sélection en mode visuel
+        vim.keymap.set('v', _G.keybinds.comment.comment, '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', { desc = 'Toggle comment for selected block' })
     end
 }
 
